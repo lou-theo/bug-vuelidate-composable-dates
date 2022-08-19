e091be00eb6a6239ad16a8f3064f90a1e80d9bed
Lou-Theo <laurent.lt@sfeir.com>
2022-08-19 11:09:00 +0200

    add working example with number

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <InputComponent v-model:value="threshold" type="date" />
-  <div>threshold : {{ threshold }}</div>
-  <InputComponent :rules="rules" type="date" />
+  <div>
+    <InputComponent v-model:value="dateThreshold" type="date" />
+    <div>threshold : {{ dateThreshold }}</div>
+    <InputComponent :rules="dateRules" type="date" />
+  </div>
+  <div>
+    <InputComponent v-model:value="numberThreshold" type="number" />
+    <div>threshold : {{ numberThreshold }}</div>
+    <InputComponent :rules="numberRules" type="number" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import InputComponent from "@/components/InputComponent.vue";
 import type { MaybeRef } from "@/types/maybeRef";
 import type { ValidationRule } from "@vuelidate/core";
-import { helpers } from "@vuelidate/validators";
+import { helpers, minValue } from "@vuelidate/validators";
 import type { Ref } from "vue";
 import { computed, ref, unref } from "vue";
 
-const threshold: Ref<string | null> = ref(null);
+const dateThreshold: Ref<string | null> = ref(null);
+const numberThreshold: Ref<number | null> = ref(null);
 
 const minDateWithoutMessage =
   (param: MaybeRef<string>) => (value: string | undefined) =>
@@ -25,10 +33,24 @@ const minDate = (param: MaybeRef<string>) =>
     minDateWithoutMessage(param)
   );
 
-const rules = computed(() => {
+const minValueMessage = (param: MaybeRef<number>) =>
+  helpers.withMessage(
+    `custom message greater than ${unref(param)}`,
+    minValue(param)
+  );
+
+const dateRules = computed(() => {
   const rules: ValidationRule[] = [];
-  if (threshold.value) {
-    rules.push(minDate(threshold.value));
+  if (dateThreshold.value) {
+    rules.push(minDate(dateThreshold.value));
+  }
+  return rules;
+});
+
+const numberRules = computed(() => {
+  const rules: ValidationRule[] = [];
+  if (numberThreshold.value) {
+    rules.push(minValueMessage(numberThreshold.value));
   }
   return rules;
 });
